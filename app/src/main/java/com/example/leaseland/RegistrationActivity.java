e1ca25f0a3c31bba5d00d34c2f9a31034937ba71
ibnahmadbello <justforlearningcode@gmail.com>
2022-05-06 22:39:54 +0100

    Registration Intent to Login Activity

--- app/src/main/java/com/example/leaseland/RegistrationActivity.java
+++ app/src/main/java/com/example/leaseland/RegistrationActivity.java
@@ -2,7 +2,11 @@ package com.example.leaseland;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        TextView login = findViewById(R.id.login_link);
+        login.setMovementMethod(LinkMovementMethod.getInstance());
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
