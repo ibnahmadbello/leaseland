2321efc7b3dd3f4e00e2c3f9ff2aef830344ceb5
ibnahmadbello <justforlearningcode@gmail.com>
2022-05-11 10:06:57 +0100

    Added signout logic to HomeActivity

--- app/src/main/java/com/example/leaseland/HomeActivity.java
+++ app/src/main/java/com/example/leaseland/HomeActivity.java
@@ -2,13 +2,33 @@ package com.example.leaseland;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button signOutButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        signOutButton = findViewById(R.id.sign_out_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+
     }
 }
