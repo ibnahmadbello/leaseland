61420bab78cdbfbe9cc3c4ffe10a36531551a56d
ibnahmadbello <justforlearningcode@gmail.com>
2022-05-11 09:43:04 +0100

    Created firebase instance to allow new user register in  Signup Activity

--- app/src/main/java/com/example/leaseland/SignupActivity.java
+++ app/src/main/java/com/example/leaseland/SignupActivity.java
@@ -1,20 +1,31 @@
 package com.example.leaseland;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
     private EditText mEmail, mPass;
     private TextView mTextView;
     private Button signUpButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,8 @@ public class SignupActivity extends AppCompatActivity {
         mTextView = findViewById(R.id.login_link);
         signUpButton = findViewById(R.id.register_button);
 
+        mAuth = FirebaseAuth.getInstance();
+
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,17 +47,41 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-//        login.setMovementMethod(LinkMovementMethod.getInstance());
-//        login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+            }
+        });
     }
 
     private void createUser(){
+        String email = mEmail.getText().toString();
+        String password = mPass.getText().toString();
 
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!password.isEmpty()){
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignupActivity.this, "Registered successfully !!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignupActivity.this, "Registrattion Failed !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                mPass.setError("Empty Fields are not Allowed");
+            }
+        } else if (email.isEmpty()){
+            mEmail.setError("Empty fields are not allowed");
+        } else {
+            mEmail.setError("Please enter a valid email address");
+        }
     }
 }
