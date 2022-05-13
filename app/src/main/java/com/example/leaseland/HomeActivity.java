e540087324a6b4f3421f14c88e11ad80bab57c36
ibnahmadbello <justforlearningcode@gmail.com>
2022-05-13 21:41:34 +0100

    Fixed link to BioData Activity

--- app/src/main/java/com/example/leaseland/HomeActivity.java
+++ app/src/main/java/com/example/leaseland/HomeActivity.java
@@ -9,9 +9,9 @@ import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button signOutButton;
+    private Button signOutButton, biodataButton;
     private FirebaseAuth mAuth;
 
     @Override
@@ -19,16 +19,26 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        biodataButton = findViewById(R.id.biodata_button);
+        biodataButton.setOnClickListener(this);
+
         signOutButton = findViewById(R.id.sign_out_button);
         mAuth = FirebaseAuth.getInstance();
 
-        signOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        signOutButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.biodata_button:
+                startActivity(new Intent(this, BioDataActivity.class));
+                break;
+            case R.id.sign_out_button:
                 mAuth.signOut();
                 startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-            }
-        });
-
+                break;
+        }
     }
 }
