9d19297607d5e188737d407664f6c783d2dd3ce9
ibnahmadbello <justforlearningcode@gmail.com>
2022-05-23 22:37:43 +0100

    Guarantor model class added

---
+++ app/src/main/java/com/example/leaseland/Guarantor.java
@@ -0,0 +1,42 @@
+package com.example.leaseland;
+
+public class Guarantor {
+
+    private String guarantorFullName;
+    private String guarantorPhoneNumber;
+    private String guarantorEmail;
+    private String guarantorResidentialAddress;
+
+    public String getGuarantorFullName() {
+        return guarantorFullName;
+    }
+
+    public void setGuarantorFullName(String guarantorFullName) {
+        this.guarantorFullName = guarantorFullName;
+    }
+
+    public String getGuarantorPhoneNumber() {
+        return guarantorPhoneNumber;
+    }
+
+    public void setGuarantorPhoneNumber(String guarantorPhoneNumber) {
+        this.guarantorPhoneNumber = guarantorPhoneNumber;
+    }
+
+    public String getGuarantorEmail() {
+        return guarantorEmail;
+    }
+
+    public void setGuarantorEmail(String guarantorEmail) {
+        this.guarantorEmail = guarantorEmail;
+    }
+
+    public String getGuarantorResidentialAddress() {
+        return guarantorResidentialAddress;
+    }
+
+    public void setGuarantorResidentialAddress(String guarantorResidentialAddress) {
+        this.guarantorResidentialAddress = guarantorResidentialAddress;
+    }
+
+}
---
+++ app/src/main/java/com/example/leaseland/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.example.leaseland;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText mEmail, mPass;
+    private TextView mTextView, forgetPasswordTextView;
+    private Button signInButton;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.login_email_text);
+        mPass = findViewById(R.id.login_password_text);
+        signInButton = findViewById(R.id.login_button);
+        mTextView = findViewById(R.id.register_link);
+        forgetPasswordTextView = findViewById(R.id.forget_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        forgetPasswordTextView.setOnClickListener(this);
+        mTextView.setOnClickListener(this);
+        signInButton.setOnClickListener(this);
+
+    }
+
+    private void loginUser(){
+        String email = mEmail.getText().toString();
+        String password = mPass.getText().toString();
+
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!password.isEmpty()){
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Login Failed !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                mPass.setError("Empty Fields are not Allowed");
+            }
+        } else if (email.isEmpty()){
+            mEmail.setError("Empty fields are not allowed");
+        } else {
+            mEmail.setError("Please enter a valid email address");
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.forget_password:
+                forgetPassword();
+                break;
+            case R.id.register_link:
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+                break;
+            case R.id.login_button:
+                loginUser();
+                break;
+        }
+    }
+
+    private void forgetPassword() {
+        startActivity(new Intent(this, ForgetPasswordActivity.class));
+        finish();
+    }
+}
