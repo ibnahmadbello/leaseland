c7164df9dacb56ca0c6a6a3995afc84612a3f95f
ibnahmadbello <justforlearningcode@gmail.com>
2022-05-25 15:27:59 +0100

    UserAgreement class added

---
+++ app/src/main/java/com/example/leaseland/utils/UserAgreement.java
@@ -0,0 +1,11 @@
+package com.example.leaseland.utils;
+
+import com.example.leaseland.GuarantorActivity;
+
+public class UserAgreement {
+
+
+
+    public UserAgreement(GuarantorActivity guarantorActivity) {
+    }
+}
