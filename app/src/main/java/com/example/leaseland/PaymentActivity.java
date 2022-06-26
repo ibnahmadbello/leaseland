9f92795bef3f53d41df6a0414faf4fdd41fc7d83
ibnahmadbello <justforlearningcode@gmail.com>
2022-06-26 02:02:18 +0100

    Initiated remita payment but it keeps failing

--- app/src/main/java/com/example/leaseland/PaymentActivity.java
+++ app/src/main/java/com/example/leaseland/PaymentActivity.java
@@ -3,12 +3,22 @@ package com.example.leaseland;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class PaymentActivity extends AppCompatActivity implements View.OnClickListener {
+import com.remita.paymentsdk.core.RemitaInlinePaymentSDK;
+import com.remita.paymentsdk.data.PaymentResponse;
+import com.remita.paymentsdk.listener.RemitaGatewayPaymentResponseListener;
+import com.remita.paymentsdk.module.MainActivity;
+import com.remita.paymentsdk.util.JsonUtil;
+import com.remita.paymentsdk.util.RIPGateway;
+
+public class PaymentActivity extends AppCompatActivity implements View.OnClickListener, RemitaGatewayPaymentResponseListener {
 
     private TextView generateRRR, webviewRRR;
+    RemitaInlinePaymentSDK remitaInlinePaymentSDK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,7 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
         generateRRR.setOnClickListener(this);
         webviewRRR.setOnClickListener(this);
 
+        remitaInlinePaymentSDK = RemitaInlinePaymentSDK.getInstance();
     }
 
     @Override
@@ -31,7 +42,20 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
                 break;
             case R.id.webviewRRR:
                 //TODO
+                String url = RIPGateway.Endpoint.DEMO;
+                String api_key = "QzAwMDAxNjMwNzl8NDA4NDEyMjQ0MHw0ODZkYTZkOTE4NTVhNzMzZmIzZTM5MTU2ZDBjZDYxY2Y4MzY4ODQ1NzRkYzIyOTI2OWQzMTU1M2NlNzdkNGZkZGIyNjI1MzA1ZjZkNzkzYzM2NTE4NzUxNTI0OWVjYjAxODUyNGZmYTM3NjY3M2IwZWNjYTU3OWEwYjE5NGMyNQ==";
+                String rrr = "220673497853";
+
+
+                remitaInlinePaymentSDK.setRemitaGatewayPaymentResponseListener(PaymentActivity.this);
+                remitaInlinePaymentSDK.initiatePayment(PaymentActivity.this, url, api_key, rrr);
                 break;
         }
+    }
+
+    @Override
+    public void onPaymentCompleted(PaymentResponse paymentResponse) {
+        Log.v("+++ Response: ", JsonUtil.toJson(paymentResponse));
+        Toast.makeText(this, JsonUtil.toJson(paymentResponse), Toast.LENGTH_SHORT).show();
     }
 }
