0c21668271909483fe64066011e1a09ffd5b7f55
ibnahmadbello <justforlearningcode@gmail.com>
2022-06-27 23:00:19 +0100

    Change implementation of sha512 hashing

--- app/src/main/java/com/example/leaseland/PaymentActivity.java
+++ app/src/main/java/com/example/leaseland/PaymentActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -12,19 +13,37 @@ import android.widget.Toast;
 import com.remita.paymentsdk.core.RemitaInlinePaymentSDK;
 import com.remita.paymentsdk.data.PaymentResponse;
 import com.remita.paymentsdk.listener.RemitaGatewayPaymentResponseListener;
-import com.remita.paymentsdk.module.MainActivity;
 import com.remita.paymentsdk.util.JsonUtil;
 import com.remita.paymentsdk.util.RIPGateway;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Date;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class PaymentActivity extends AppCompatActivity implements View.OnClickListener, RemitaGatewayPaymentResponseListener {
 
     private TextView generateRRR, webviewRRR;
     RemitaInlinePaymentSDK remitaInlinePaymentSDK;
+//    public String url= "https://reqres.in/api/users/2";
+    String url = "https://remitademo.net/remita/exapp/api/v1/send/api/echannelsvc/merchant/api/paymentinit";
+    String returnedHash = "";
+    String merchantId = "";
+    long orderId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,45 +64,34 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
         switch (view.getId()){
             case R.id.generateRRR:
                 //TODO
-                String merchantId = "2547916";
+                merchantId = "2547916";
                 String apiKey = "1946";
                 String serviceTypeId = "4430731";
                 Date d = new Date();
-                long orderId = d.getTime();
+                orderId = d.getTime();
                 String totalAmount = "1000";
                 try {
-                    MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
                     String input = merchantId+ serviceTypeId+ orderId+totalAmount+apiKey;
-                    byte[] md = messageDigest.digest(input.getBytes());
-                    BigInteger no = new BigInteger(1, md);
-                    String hashInput = no.toString(16);
-                    while (hashInput.length() < 32){
-                        hashInput = "0" + hashInput;
+                    MessageDigest md = MessageDigest.getInstance("SHA-512");
+                    byte[] messageDigest = md.digest(input.getBytes());
+                    // Convert byte array into signum representation
+                    BigInteger no = new BigInteger(1, messageDigest);
+                    String returnedHash = no.toString(16);
+                    while (returnedHash.length() < 32){
+                        returnedHash = "0" + returnedHash;
                     }
+                    Toast.makeText(this, "+++"+ returnedHash, Toast.LENGTH_SHORT).show();
                 } catch (NoSuchAlgorithmException e) {
                     Log.d("Hash Error", e.getMessage());
                     Toast.makeText(this, "Hash error" + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
+
+                try {
+                    run();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
 //                var apiHash = CryptoJS.SHA512(merchantId+ serviceTypeId+ orderId+totalAmount+apiKey);
-//            {
-//                "serviceTypeId": "{{serviceTypeId}}",
-//                    "amount": "{{totalAmount}}",
-//                    "orderId": "{{orderId}}",
-//                    "payerName": "Michelle Alozie",
-//                    "payerEmail": "alozie@systemspecs.com.ng",
-//                    "payerPhone": "09062067384",
-//                    "description": "Payment for Donation 3",
-//                    "customFields":[{
-//                "name":"Matric Number",
-//                        "value":"1509329285795",
-//                        "type":"ALL"
-//            },
-//                {
-//                    "name":"Invoice Number",
-//                        "value":"1234",
-//                        "type":"ALL"
-//                }]
-//            }
                 break;
             case R.id.webviewRRR:
                 //TODO
@@ -98,6 +106,71 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (int i =0; i < bytes.length; i++){
+            String hex = Integer.toHexString(0xff & bytes[i]);
+            if (hex.length() == 1){
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    void run() throws IOException{
+        OkHttpClient client = new OkHttpClient();
+
+        JSONObject bodyPost = new JSONObject();
+        try {
+            bodyPost.put("serviceTypeId", "4430731");
+            bodyPost.put("amount", "10000");
+            bodyPost.put("orderId", orderId);
+            bodyPost.put("payerName", "Arab");
+            bodyPost.put("payerEmail", "justforlearningcode@gmail.com");
+            bodyPost.put("payerPhone", "08123447855");
+            bodyPost.put("description", "Rent on Land");
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(bodyPost.toString(), JSON);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+//                .method("POST", body)
+                .header("Content-Type", "application/json")
+//                .header("Accept", "application/json")
+                .header("Authorization", "remitaConsumerKey="+merchantId+",remitaConsumerToken="+returnedHash+"")
+//                .header("Authorization", remitaConsumerToken)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+
+                PaymentActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        generateRRR.setText(myResponse);
+                        Toast.makeText(PaymentActivity.this, "---"+myResponse, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+
     @Override
     public void onPaymentCompleted(PaymentResponse paymentResponse) {
         Log.v("+++ Response: ", JsonUtil.toJson(paymentResponse));
@@ -105,4 +178,12 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
         if (paymentResponse.getResponseMessage()=="SUCCESS")
             startActivity(new Intent(this, HomeActivity.class));
     }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
 }
---
+++ app/src/main/java/com/example/leaseland/utils/NetworkCall.java
@@ -0,0 +1,4 @@
+package com.example.leaseland.utils;
+
+public class NetworkCall {
+}
